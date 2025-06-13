97ab3e382c3eb80820b15b2aecc7138665448a6e
avisink <ayomidevanessa@gmail.com>
2025-06-13 11:52:09 -0700

    Removed placeholder text and added prop types to correctly access needed fields

--- src/MovieCard/MovieCard.jsx
+++ src/MovieCard/MovieCard.jsx
@@ -1,16 +1,27 @@
 import "../MovieCard/MovieCard.css";
+import "/Users/aisinkaye/codepath/flixster-unit3/src/App.css";
+import PropTypes from "prop-types";
 
-
-function MovieCard() {
+function MovieCard({ movie }) {
     return (
       <div>
         <div className="movie-card">
-          <img src="" alt="movie poster image" />
-          <p>Movie Title Placeholder</p>
-          <p>MOvie Rating Placeholder</p>
+          <img src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`} alt={`${movie.title} poster`}
+          />
+          <p>{movie.title}</p>
+          <p>Movie Rating: {movie.vote_average}</p>
         </div>
       </div>
     );
+}
+
+
+MovieCard.propTypes = {
+    movie: PropTypes.shape({
+        title: PropTypes.string.isRequired,
+        vote_average: PropTypes.number.isRequired,
+        poster_path: PropTypes.string.isRequired,
+    }).isRequired
 }
 
 export default MovieCard;
