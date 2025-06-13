b5263e53c0b01ee79a3a8fa66fd47d7dd69d549c
avisink <ayomidevanessa@gmail.com>
2025-06-12 23:21:31 -0700

    Made MovieCard folder with jsx and css components in it. This is for each movie tile/card that will be displayed in the movie list.

---
+++ src/MovieCard/MovieCard.jsx
@@ -0,0 +1,16 @@
+import "../MovieCard/MovieCard.css";
+
+
+function MovieCard() {
+    return (
+      <div>
+        <div className="movie-card">
+          <img src="" alt="movie poster image" />
+          <p>Movie Title Placeholder</p>
+          <p>MOvie Rating Placeholder</p>
+        </div>
+      </div>
+    );
+}
+
+export default MovieCard;
