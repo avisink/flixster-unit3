c8ea4dcd6e987edbe09605f4827fa384a2fddf45
avisink <ayomidevanessa@gmail.com>
2025-06-13 11:55:00 -0700

    Added comments for the hooks for learning references. Added try catch for error handling as learned in class

--- src/MovieList/MovieList.jsx
+++ src/MovieList/MovieList.jsx
@@ -1,18 +1,26 @@
 import { useEffect, useState } from "react";
 import MovieCard from "../MovieCard/MovieCard.jsx";
-// import MovieCard from "src/MovieCard/MovieCard.jsx";
+import "../App.css";
+import "../MovieList/MovieList.css";
 
+
+// hook
 function MovieList() {
     const [movies, setMovies] = useState([]);
 
-    // fucntion to fetch movies
+    // fucntion to fetch movies - another hok
     useEffect (() => {
         async function fetchMovies() {
             const apiKey = import.meta.env.VITE_API_KEY;
             const url = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}`; 
             const response = await fetch(url);
-            const data = await response.json();
-            setMovies(data.results);
+            try {
+                const data = await response.json();
+                setMovies(data.results);
+            } catch (err) {
+                console.error("Error fetching movie list: ", err);
+            }
+            
         }
         fetchMovies();
 
@@ -20,14 +28,14 @@ function MovieList() {
     
   return (
     <>  
-        {movies.map((movie) => {
+        {movies.map((movie) => (
             <MovieCard 
             key= {movie.id}
             movie={movie}
             title={movie.title}
             vote_average ={movie.vote_average}
             />
-        })}
+        ))}
     </>
   );
 }
