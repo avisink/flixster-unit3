48fb17f7e40d15a639ac3ed2e1cf47914b18fbbb
avisink <ayomidevanessa@gmail.com>
2025-06-16 14:33:17 -0700

    Added like feature, originally set to vote count from tmdb

--- src/MovieCard/MovieCard.jsx
+++ src/MovieCard/MovieCard.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "../MovieCard/MovieCard.css";
 import PropTypes from "prop-types";
 
@@ -5,20 +6,34 @@ function MovieCard({ movie, onClick }) {
   const imageUrl = movie.poster_path
     ? `https://image.tmdb.org/t/p/w200${movie.poster_path}`
     : null;
+  const [liked, setLiked] = useState(false);
+  const [likes, setLikes] = useState(movie.vote_count);
 
+  const handleHeartClick =(e) => {
+    e.stopPropagation();
+    if (liked) {
+      setLiked(false);
+      setLikes(movie.vote_count);
+    } else {
+      setLiked(true);
+      setLikes(movie.vote_count +1);
+    }
+  }
+ 
   return (
     <div>
       <div className="movie-card" onClick={onClick}>
         {imageUrl ? (
-          <img
-            src={imageUrl}
-            alt={`${movie.title} poster`}
-          />
+          <img src={imageUrl} alt={`${movie.title} poster`} />
         ) : (
           <div className="img-placeholder">No Image</div>
         )}
         <p>{movie.title}</p>
         <p>Movie Rating: {movie.vote_average.toFixed(1)}</p>
+        <span className="heart-icon" role="button" aria-label="like" onClick={handleHeartClick}>
+          {liked ? "💚":"🤍"}
+        </span>
+        <span className="like-count">{likes}</span>
       </div>
     </div>
   );
@@ -28,6 +43,7 @@ MovieCard.propTypes = {
   movie: PropTypes.shape({
     title: PropTypes.string.isRequired,
     vote_average: PropTypes.number.isRequired,
+    vote_count: PropTypes.number.isRequired,
     poster_path: PropTypes.string,
   }).isRequired,
   onClick: PropTypes.func.isRequired,
