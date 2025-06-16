614a9fe5193940003a411afedc13be057ec512b2
avisink <ayomidevanessa@gmail.com>
2025-06-16 13:34:45 -0700

    Added correct image source, by accessing movie poster path. Also added image placeholder in case there is no poster. Added onlick to props to make all cards clickable

--- src/MovieCard/MovieCard.jsx
+++ src/MovieCard/MovieCard.jsx
@@ -1,30 +1,36 @@
 import "../MovieCard/MovieCard.css";
-import "../App.css";
-
 import PropTypes from "prop-types";
 
-function MovieCard({ movie }) {
-    return (
-      <div>
-        <div className="movie-card">
+function MovieCard({ movie, onClick }) {
+  const imageUrl = movie.poster_path
+    ? `https://image.tmdb.org/t/p/w200${movie.poster_path}`
+    : null;
+
+  return (
+    <div>
+      <div className="movie-card" onClick={onClick}>
+        {imageUrl ? (
           <img
-            src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
+            src={imageUrl}
             alt={`${movie.title} poster`}
           />
-          <p>{movie.title}</p>
-          <p>Movie Rating: {movie.vote_average.toFixed(1)}</p>
-        </div>
+        ) : (
+          <div className="img-placeholder">No Image</div>
+        )}
+        <p>{movie.title}</p>
+        <p>Movie Rating: {movie.vote_average.toFixed(1)}</p>
       </div>
-    );
+    </div>
+  );
 }
-
 
 MovieCard.propTypes = {
-    movie: PropTypes.shape({
-        title: PropTypes.string.isRequired,
-        vote_average: PropTypes.number.isRequired,
-        poster_path: PropTypes.string.isRequired,
-    }).isRequired
-}
+  movie: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    vote_average: PropTypes.number.isRequired,
+    poster_path: PropTypes.string,
+  }).isRequired,
+  onClick: PropTypes.func.isRequired,
+};
 
 export default MovieCard;
