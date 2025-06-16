d4a3c6db7f087fdcffb388763a1dbb9e522fd903
avisink <ayomidevanessa@gmail.com>
2025-06-16 15:33:27 -0700

    Added favorite-ing and watch(ed) feature, with visual elements/icons for them

--- src/MovieCard/MovieCard.jsx
+++ src/MovieCard/MovieCard.jsx
@@ -1,12 +1,19 @@
 import { useState } from "react";
 import "../MovieCard/MovieCard.css";
 import PropTypes from "prop-types";
+import { FaCheckCircle, FaRegCircle } from "react-icons/fa";
+import "react-tooltip/dist/react-tooltip.css";
+import { Tooltip } from "react-tooltip";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBookmark as faBookmarkSolid } from "@fortawesome/free-solid-svg-icons";
+import { faBookmark as faBookmarkRegular } from "@fortawesome/free-regular-svg-icons";
 
 function MovieCard({ movie, onClick }) {
   const imageUrl = movie.poster_path
     ? `https://image.tmdb.org/t/p/w200${movie.poster_path}`
     : null;
   const [liked, setLiked] = useState(false);
+  const [isWatched, setIsWatched] = useState(false);
 
   const handleHeartClick =(e) => {
     e.stopPropagation();
@@ -16,6 +23,11 @@ function MovieCard({ movie, onClick }) {
       setLiked(false);
     }
   }
+
+  const toggleWatched = (e) => {
+    e.stopPropagation();
+    setIsWatched(!isWatched);
+  };
  
   return (
     <div>
@@ -27,9 +39,38 @@ function MovieCard({ movie, onClick }) {
         )}
         <p>{movie.title}</p>
         <p>Movie Rating: {movie.vote_average.toFixed(1)}</p>
-        <span className="heart-icon" role="button" aria-label="like" onClick={handleHeartClick}>
-          {liked ? "💚":"🤍"}
-        </span>
+        <div>
+          <span
+            className="heart-icon"
+            role="button"
+            aria-label="like"
+            onClick={handleHeartClick}
+            data-tooltip-id="favorited-tooltip"
+            data-tooltip-content={
+              liked ? "Added to favorites!" : "Add to favorites."
+            }
+          >
+            {liked ? (
+              <FontAwesomeIcon icon={faBookmarkSolid} />
+            ) : (
+              <FontAwesomeIcon icon={faBookmarkRegular} />
+            )}
+          </span>
+          <Tooltip id="favorited-tooltip" />
+
+          <span
+            data-tooltip-id="watched-tooltip"
+            data-tooltip-content={isWatched ? "Watched" : "Mark as watched"}
+            onClick={toggleWatched}
+          >
+            {isWatched ? (
+              <FaCheckCircle color="#22c55e" />
+            ) : (
+              <FaRegCircle color="#64748b" />
+            )}
+          </span>
+          <Tooltip id="watched-tooltip" />
+        </div>
       </div>
     </div>
   );
