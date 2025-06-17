45a75d407a1fac77143ec605dee85d7bb0308781
avisink <ayomidevanessa@gmail.com>
2025-06-17 11:07:08 -0700

    Made new container around certain elemnts to allow more styling

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -32,27 +32,40 @@ function Header({ onSearch, onSort, searchQuery, sortBy }) {
       <>
         <header>
           <h1>Movies</h1>
+          <div>
+            <form onSubmit={handleSubmit}>
+              <label>Search: </label>
+              <input
+                type="text"
+                value={input}
+                onChange={(e) => setInput(e.target.value)}
+                onKeyPress={handleEnterClick}
+                className="search-input"
+                placeholder="Search for movies..."
+              ></input>
+              <button type="submit" id="submitBtn">
+                Submit
+              </button>
 
-          <form onSubmit={handleSubmit}>
-            <label>Search: </label>
-            <input type="text" value={input} onChange={(e) => setInput(e.target.value)} onKeyPress={handleEnterClick} className="search-input" placeholder="Search for movies..."></input>
-            <button type="submit" id="submitBtn">
-              Submit
-            </button>
+              <button type="submit" id="clearBtn" onClick={handleClear}>
+                Clear
+              </button>
 
-            <button type="submit" id="clearBtn" onClick={handleClear}>
-              Clear 
-            </button>
-            
-            <div>
-              <label>Sort by</label>
-              <select name="movies" id="movie-sorting" value={sortBy} onChange={handleSortBy}>
-                <option value="popularity">Popularity</option>
-                <option value="title">Title</option>
-                <option value="release_date">Release Date</option>
-              </select>
-            </div>
-          </form>
+              <div>
+                <label>Sort by</label>
+                <select
+                  name="movies"
+                  id="movie-sorting"
+                  value={sortBy}
+                  onChange={handleSortBy}
+                >
+                  <option value="popularity">Popularity</option>
+                  <option value="title">Title</option>
+                  <option value="release_date">Release Date</option>
+                </select>
+              </div>
+            </form>
+          </div>
         </header>
       </>
     );
